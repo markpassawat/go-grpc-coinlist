3bd7f96f4e3f12a407695cf15997e0047fbf3a04
markpassawat <markpassawat.dev@gmail.com>
2022-06-12 00:18:20 +0700

    fix: reslove warning from for ticker.c

--- pkg/coingecko/db/update_coin_price.go
+++ pkg/coingecko/db/update_coin_price.go
@@ -14,7 +14,7 @@ import (
 
 func UpdateCoinPrice() {
 	ticker := time.NewTicker(time.Hour)
-	for _ = range ticker.C {
+	for range ticker.C {
 		db := db.ConnectDatabase()
 		ctx := context.TODO()
 
