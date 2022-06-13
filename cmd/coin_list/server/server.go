af6a5d82494b0bbf95641aab3e61bb5df05a216f
markpassawat <markpassawat.dev@gmail.com>
2022-06-13 15:31:55 +0700

    fix: clean code and restructure files

--- cmd/coin_list/server/server.go
+++ cmd/coin_list/server/server.go
@@ -8,13 +8,14 @@ import (
 	"google.golang.org/grpc"
 
 	"github.com/markpassawat/go-grpc-coinlist/cmd/coin_list/config"
-	cgk "github.com/markpassawat/go-grpc-coinlist/pkg/coingecko/svc"
-	db "github.com/markpassawat/go-grpc-coinlist/pkg/coingecko/db"
+	// cgk "github.com/markpassawat/go-grpc-coinlist/pkg/coingecko/svc"
+	dbCon "github.com/markpassawat/go-grpc-coinlist/pkg/common/db"
 	pb "github.com/markpassawat/go-grpc-coinlist/proto/coinlist"
 )
 
 func Handler(cfg *config.Config) (*grpc.Server, net.Listener) {
-	go db.UpdateCoinPrice()
+
+	// go db.UpdateCoinPrice()
 	// Create a listener on TCP port
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", cfg.Host, cfg.Port))
 	if err != nil {
@@ -24,7 +25,9 @@ func Handler(cfg *config.Config) (*grpc.Server, net.Listener) {
 	// Create a gRPC server object
 	s := grpc.NewServer()
 	// Attach the Greeter service to the server
-	pb.RegisterCoinListServer(s, &cgk.CoinServer{})
+	pb.RegisterCoinListServer(s, &CoinServer{
+		db: dbCon.ConnectDatabase(),
+	})
 
 	return s, lis
 }
